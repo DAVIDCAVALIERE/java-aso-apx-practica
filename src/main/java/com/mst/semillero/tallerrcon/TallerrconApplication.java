47c0bf1cfe2253758ed8392fe85df12cfbd4de36
cavaliere <davincicavaliere@gmail.com>
2025-06-09 21:57:14 -0500

    estructura inicial

---
+++ src/main/java/com/mst/semillero/tallerrcon/TallerrconApplication.java
@@ -0,0 +1,13 @@
+package com.mst.semillero.tallerrcon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerrconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerrconApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mst/semillero/tallerrcon/TallerrconApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mst.semillero.tallerrcon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerrconApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
